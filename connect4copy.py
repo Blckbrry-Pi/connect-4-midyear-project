9aca470989bf542a66a4d08ec60432d21e1b199a
Alice Zhang <anz0063@gmail.com>
2021-02-18 21:19:33 -0500

    added winning logic to connect4copy.py

--- connect4copy.py
+++ connect4copy.py
@@ -1,5 +1,5 @@
 import arcade
-
+import copy
 
 #import token
 #player1 = Token()
@@ -156,7 +156,6 @@ height = 85
 margins = 5
 screenWidth = (width + margins) * columnCount + margins
 screenHeight = (height + margins) * rowCount + margins
-title = "Connect 4"
 turn = 1
 turnTxt = "Player 1's turn"
 
@@ -174,23 +173,201 @@ class Connect4(arcade.View):
 
     def setup(self):
         pass
+    
+    def horizWin(self,grid):
+        win=[0,False]
 
+        p1tokens=0
+        p2tokens=0
+        for row in grid:
+            for i in range(len(row)):
+                if row[i]==1:
+                    p1tokens+=1
+                    p2tokens=0
+                    if p1tokens>=4:
+                        win=[1,True]
+                elif row[i]==2:
+                    p1tokens=0
+                    p2tokens+=1
+                    if p2tokens>=4:
+                        win=[2,True]
+                elif row[i]==0:
+                    p1tokens=0
+                    p2tokens=0
+            p1tokens=0
+            p2tokens=0
+    
+        return win
+            
+
+    def vertWin(self,grid):
+        win=[0,False]
+
+        p1tokens=0
+        p2tokens=0
+
+        for i in range(len(grid[0])):
+            for row in grid:
+                if row[i]==1:
+                    p1tokens+=1
+                    p2tokens=0
+                    if p1tokens>=4:
+                        win=[1,True]
+                elif row[i]==2:
+                    p1tokens=0
+                    p2tokens+=1
+                    if p2tokens>=4:
+                        win=[2,True]
+                elif row[i]==0:
+                    p1tokens=0
+                    p2tokens=0
+            p1tokens=0
+            p2tokens=0
+
+        return win
+
+    def diagWin1(self,grid):
+        win=[0,False]
+
+        p1tokens=0
+        p2tokens=0
+        i = 0
+        for column in range (len(grid[0]) - 2):
+            for row in grid:
+                if i > 6:
+                    continue
+                elif row[i]==1:
+                    p1tokens+=1
+                    p2tokens=0
+                    if p1tokens>=4:
+                        win=[1,True]
+                elif row[i]==2:
+                    p1token=0
+                    p2tokens+=1
+                    if p2tokens>=4:
+                        win=[2,True]
+                elif row[i]==0:
+                    p1tokens=0
+                    p2tokens=0
+                i += 1
+            i = column
+            p1tokens=0
+            p2tokens=0
+
+
+
+        i = 0
+        for r in range(len(grid[1:4])):
+            for row in grid[r:]:
+                if row[i]==1:
+                    p1tokens+=1
+                    p2tokens=0
+                    if p1tokens>=4:
+                        win=[1,True]
+                elif row[i]==2:
+                    p1token=0
+                    p2tokens+=1
+                    if p2tokens>=4:
+                        win=[2,True]
+                elif row[i]==0:
+                    p1tokens=0
+                    p2tokens=0
+                i += 1
+            i = 0
+            p1tokens=0
+            p2tokens=0
+
+        return win 
+
+    def diagWin2(self,grid):
+        win=[0,False]
+        gridTemp = copy.deepcopy(grid)
+        p1tokens=0
+        p2tokens=0
+        i=0
+
+        for i in range (len(gridTemp)):
+            gridTemp[i].reverse()
+
+        for column in range (len(gridTemp[0]) - 2):
+            for row in gridTemp:
+                if i > 6:
+                    continue
+                elif row[i]==1:
+                    p1tokens+=1
+                    p2tokens=0
+                    if p1tokens>=4:
+                        win=[1,True]
+                elif row[i]==2:
+                    p1token=0
+                    p2tokens+=1
+                    if p2tokens>=4:
+                        win=[2,True]
+                elif row[i]==0:
+                    p1tokens=0
+                    p2tokens=0
+                i += 1
+
+            i = column
+            p1tokens=0
+            p2tokens=0
+
+
+
+        i = 0
+        for r in range(len(gridTemp[1:4])):
+            for row in gridTemp[r:]:
+                if row[i]==1:
+                    p1tokens+=1
+                    p2tokens=0
+                    if p1tokens>=4:
+                        win=[1,True]
+                elif row[i]==2:
+                    p1token=0
+                    p2tokens+=1
+                    if p2tokens>=4:
+                        win=[2,True]
+                elif row[i]==0:
+                    p1tokens=0
+                    p2tokens=0
+                i += 1
+            i = 0
+            p1tokens=0
+            p2tokens=0
+
+        return win 
+        
+    def win(self, grid):
+        hor = self.horizWin(grid)
+        vert = self.vertWin(grid)
+        dia1 = self.diagWin1(grid)
+        dia2 = self.diagWin2(grid)
+        for wins in (hor,vert,dia1,dia2):
+            if wins[1] == True:
+                return wins
+        return [0,False]
+        
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
         global turn
         global turnTxt
-        for keys in ([arcade.key.KEY_0,0],[arcade.key.KEY_1,1],[arcade.key.KEY_2,2],[arcade.key.KEY_3,3],[arcade.key.KEY_4,4],[arcade.key.KEY_5,5],[arcade.key.KEY_6,6]):
-            if key == keys[0]:
-                for rowIndex in range(len(self.grid)):
-                    if self.grid[rowIndex][keys[1]] == 0:
-                        if turn % 2 == 1:
-                            self.grid[rowIndex][keys[1]] = 1
-                            turnTxt = "Player 2's turn"
-                        else:
-                            self.grid[rowIndex][keys[1]] = 2
-                            turnTxt = "Player 1's turn"
-                        turn += 1
-                        break
+
+        if not (self.win(self.grid)[1]):
+            for keys in ([arcade.key.KEY_0,0],[arcade.key.KEY_1,1],[arcade.key.KEY_2,2],[arcade.key.KEY_3,3],[arcade.key.KEY_4,4],[arcade.key.KEY_5,5],[arcade.key.KEY_6,6]):
+                if key == keys[0]:
+                    for rowIndex in range(len(self.grid)):
+                        if self.grid[rowIndex][keys[1]] == 0:
+                            if turn % 2 == 1:
+                                self.grid[rowIndex][keys[1]] = 1
+                                turnTxt = "Player 2's turn"
+                            else:
+                                self.grid[rowIndex][keys[1]] = 2
+                                turnTxt = "Player 1's turn"
+                            turn += 1
+                            break
+        if self.win(self.grid)[1]:
+            turnTxt = 'Player ' + str(self.win(self.grid)[0]) + ' won the game! Press Esc to play again.'
+
         if key == arcade.key.ESCAPE:
             menu_view = MenuView()
             self.window.show_view(menu_view)
@@ -201,7 +378,7 @@ class Connect4(arcade.View):
         """
         Render the screen.
         """
-
+        
         # This command has to happen before we start drawing
         arcade.start_render()
 
@@ -226,8 +403,10 @@ class Connect4(arcade.View):
         arcade.draw_rectangle_filled(WIDTH/2, 750, 700, 85, arcade.color.WHITE)
         arcade.draw_text("On your keyboard, press on the number corresponding to\nthe column you want to place your token in.\nPress Esc to reset.", WIDTH/2,715,
                          arcade.color.BLACK, font_size=20, anchor_x="center")
+
+
         arcade.draw_text(turnTxt, WIDTH/2,625,
-                         arcade.color.BLACK, font_size=20, anchor_x="center")
+                         arcade.color.BLACK, font_size=30, anchor_x="center")
         
 
     
